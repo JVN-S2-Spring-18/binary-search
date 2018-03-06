2335eac1d1fd75d1bcf36ecf4ef75684ee1a13ff
Dung Hong <dung.hong@mservice.com.vn>
2018-03-06 17:35:03 +0700

    Template for binary search.

---
+++ binary_search.py
@@ -0,0 +1,14 @@
+def binary_search(sorted_list, value):
+    """
+    Find an element in a sorted list of integers.
+
+    If the element exists in the array, return the index of the element (0-based).
+    Otherwise, return -1.
+
+    Args:
+        sorted_list: a list of integers in sorted, ascending order.
+        value: a number to search for.
+    Returns:
+        The position index (0-based) of the value if found. Otherwise, return -1.
+    """
+    return -1
